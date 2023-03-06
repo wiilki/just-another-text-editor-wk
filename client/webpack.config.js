2031b1ee1407b8f9d2ab4b36e5e4d5cf98e23e70
Will <willkim0608@gmail.com>
2023-03-06 15:52:40 -0800

    Edit name

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -20,7 +18,7 @@ module.exports = () => {
       // Webpack plugin that generates our html file and injects our bundles. 
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Online Offline Text Editor'
+        title: 'Just Another Text Editor'
       }),
 
       // Injects our custom service worker
@@ -33,9 +31,9 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Online Offline Text Editor',
-        short_name: 'Text Editor',
-        description: 'A simple text editor',
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'A simple online & offline text editor',
         background_color: '#225ca3',
         theme_color: '#225ca3',
         start_url: './',
