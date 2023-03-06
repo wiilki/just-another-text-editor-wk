6d3e7f37eddc204e3384b87573378a1b83264332
Will <willkim0608@gmail.com>
2023-03-06 14:58:55 -0800

    Edit webpack syntax

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,6 +3,8 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
+// TODO: Add and configure workbox plugins for a service worker and manifest file.
+
 module.exports = () => {
   return {
     mode: 'development',
@@ -15,32 +17,40 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      // Webpack plugin that generates our html file and injects our bundles. 
       new HtmlWebpackPlugin({
-        template: './src/install.html',
-        chunks: ['install'],
-        filename: 'install.html',
+        template: './index.html',
+        title: 'Online Offline Text Editor'
       }),
+
+      // Injects our custom service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+      // Creates a manifest.json file.
       new WebpackPwaManifest({
-        name: 'My Text Editor',
-        short_name: 'Online Offline Text Editor',
+        fingerprints: false,
+        inject: true,
+        name: 'Online Offline Text Editor',
+        short_name: 'Text Editor',
         description: 'A simple text editor',
-        background_color: '#ffffff',
-        crossorigin: 'use-credentials',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
         icons: [
           {
-            src: path.resolve('src/img/icon.png'),
+            src: path.resolve('src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join('assets', 'icons'),
           },
         ],
       }),
-      new InjectManifest({
-        swSrc: './src-sw.js',
-        exclude: [/\.map$/, /manifest\.json$/],
-      }),
     ],
 
-// Add CSS loaders and babel
+    // Add CSS loaders and babel
     module: {
       rules: [
         {
