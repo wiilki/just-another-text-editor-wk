4bf6b1de72c8fa17fc119db43c3409f46a815c11
Will <willkim0608@gmail.com>
2023-03-06 14:45:33 -0800

    Add and configure plugin for service worker and manifest file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -17,7 +15,29 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './src/install.html',
+        chunks: ['install'],
+        filename: 'install.html',
+      }),
+      new WebpackPwaManifest({
+        name: 'My Text Editor',
+        short_name: 'Online Offline Text Editor',
+        description: 'A simple text editor',
+        background_color: '#ffffff',
+        crossorigin: 'use-credentials',
+        icons: [
+          {
+            src: path.resolve('src/img/icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        exclude: [/\.map$/, /manifest\.json$/],
+      }),
     ],
 
 // Add CSS loaders and babel
