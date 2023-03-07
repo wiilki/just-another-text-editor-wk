79538df6b4c29ba623c7807423587ea44096843a
Will <willkim0608@gmail.com>
2023-03-06 16:02:01 -0800

    Clean up comments on getDb. Edit putDb in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,27 +12,25 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
 
+// Export a function we will use to PUT to the database.
+export const putDb = async (id, content) => {
+  console.log('PUT to the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: id, todo: content });
+  const result = await request;
+  console.log('result.value', result);
+};
 
 // Export a function we will use to GET to the database.
 export const getDb = async () => {
   console.log('GET from the database');
-
-  // Create a connection to the database database and version we want to use.
   const jateDb = await openDB('jate', 1);
-
-  // Create a new transaction and specify the database and data privileges.
   const tx = jateDb.transaction('jate', 'readwrite');
-
-  // Open up the desired object store.
   const store = tx.objectStore('jate');
-
-  // Use the .getAll() method to get all data in the database.
   const request = store.getAll();
-
-  // Get confirmation of the request.
   const result = await request;
   console.log('result.value', result);
   return result;
